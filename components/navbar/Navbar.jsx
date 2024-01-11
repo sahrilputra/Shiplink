a60ec0bd584ba3127a6110e08de30a82d4bd6d3a
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-01-11 16:15:24 +0700

    navbar structured

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,50 +1,83 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import Image from 'next/image'
 export const Navbar = () => {
     return (
         <>
             {/*  */}
-            <div className="w-full h-[68px] left-0 top-0 bg-white" />
-            <div className="right-[150px] top-[9px] absolute justify-end items-center gap-[54px] inline-flex">
-                <div className="justify-center items-center gap-6 flex">
-                    <div className="justify-start items-center gap-1 flex">
-                        <div className="justify-start items-center gap-1 flex">
-                            <div className="justify-start items-start gap-2.5 flex">
-                                <Image
-                                    src={"/icon/notification.svg"}
-                                    width={20}
-                                    height={20}
-                                    alt="notification icon"
-                                />
-                                <div className="justify-start items-center gap-1 flex">
-                                    <div className="text-black text-base font-normal font-['Poppins']">Notification</div>
-                                    <div className="w-6 h-6 relative" />
-                                </div>
-                            </div>
+            <div className="w-full h-[68px] left-0 top-0 bg-white" >
+                <div className="justify-end items-center gap-10 flex py-3 px-10">
+                    <button className="justify-start items-center gap-3 flex">
+                        <div className="text-black text-sm font-normal font-['Poppins']">Ship</div>
+                        <div className="relative" >
+                            <Image
+                                src={"/icon/blackArrowIcon.svg"}
+                                width={10}
+                                height={10}
+                                alt="arrow icon"
+                            />
                         </div>
-                    </div>
-                </div>
-                <div className="justify-start items-center gap-1 flex">
-                    <div className="justify-start items-center gap-2 flex">
-                        <div className="w-[18px] h-[18px] relative">
-                            <div className="w-[18px] h-[18px] left-0 top-0 absolute">
-                                <div className="w-[18px] h-[18px] left-0 top-0 absolute">
-                                </div>
-                            </div>
+                    </button>
+                    <button className="justify-start items-center gap-3 flex">
+                        <div className="text-black text-sm font-normal font-['Poppins']">Track</div>
+                        <div className="relative" >
+                            <Image
+                                src={"/icon/blackArrowIcon.svg"}
+                                width={10}
+                                height={10}
+                                alt="arrow icon"
+                            />
                         </div>
-                        <div className="text-black text-base font-semibold font-['Poppins']">Eng</div>
-                    </div>
-                    <div className="w-6 h-6 relative" />
-                </div>
-                <div className="justify-start items-center gap-1 flex">
-                    <div className="justify-start items-center gap-[13px] flex">
-                        <div className="justify-start items-center gap-2.5 flex">
-                            <div className="w-[105px] flex-col justify-start items-start inline-flex">
-                                <div className="w-[143px] text-black text-base font-semibold font-['Poppins']">Username</div>
-                                <div className="w-[143px] text-black text-sm font-light font-['Poppins']">Admin</div>
-                            </div>
+                    </button>
+                    <button className="justify-start items-center gap-3 flex">
+                        <div className="text-black text-sm font-normal font-['Poppins']">Support</div>
+                        <div className="relative" >
+                            <Image
+                                src={"/icon/blackArrowIcon.svg"}
+                                width={10}
+                                height={10}
+                                alt="arrow icon"
+                            />
                         </div>
-                    </div>
+                    </button>
+
+                    <button className="justify-start items-center gap-3 flex">
+                        <Image
+                            src={"/assets/country/uk-flag.png"}
+                            width={20}
+                            height={20}
+                            alt="uk-flag"
+                        />
+                        <div className="text-black text-sm font-semibold font-['Poppins']">Eng</div>
+                        <div className="relative" >
+                            <Image
+                                src={"/icon/blackArrowIcon.svg"}
+                                width={12}
+                                height={12}
+                                alt="arrow icon"
+                            />
+                        </div>
+                    </button>
+
+                    <button className="flex flex-row gap-3 justify-center items-center">
+                        <img
+                            src="https://source.boringavatars.com/beam"
+                            className='rounded-full w-[35px] h-[35px]'
+                            alt="user image"
+                        />
+                        <div className="flex flex-col justify-start text-left">
+                            <div className=" text-black text-sm font-semibold font-['Poppins']">User, </div>
+                            <div className=" text-black text-sm font-light font-['Poppins']">Premium</div>
+                        </div>
+                        <div className="justify-center items-center inline-flex" >
+                            <Image
+                                src={"/icon/blackArrowIcon.svg"}
+                                width={12}
+                                height={12}
+                                alt="arrow icon"
+                            />
+                        </div>
+                    </button>
                 </div>
             </div>
         </>
