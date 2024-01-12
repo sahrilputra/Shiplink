a3cd28720ef9d877c72ac52101a25b96893fb6ca
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-01-12 12:32:43 +0700

    change structured

--- app/admin/configuration/layout.jsx
+++ app/admin/configuration/layout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './styles.module.scss'
 import Image from 'next/image';
 
-import { ConfigMenus } from './_components/ConfigMenus';
+import { ConfigMenus } from './components/ConfigMenus';
 
 
 export default function ConfigurationMenus({ children }) {
