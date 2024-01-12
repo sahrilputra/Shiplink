ed3e288be873afb524b40201185bf1ae7adaa716
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-01-12 12:28:07 +0700

    change compoenetns name

--- app/admin/configuration/page.jsx
+++ app/admin/configuration/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import styles from './styles.module.scss'
-import { CarrierList } from './_configComponents/carrierList'
+import { CarrierList } from './_components/carrierList'
 // import Image from 'next/image'
 import { TextInput } from 'flowbite-react'
 import { SearchIcon } from '@/components/icons/iconCollection'
