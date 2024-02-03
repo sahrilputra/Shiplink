98508202b96071899567b036b3fbf7686030e860
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-02-03 21:19:24 +0700

    adding modals

--- app/admin/verification/components/Table/ImageDisplay.jsx
+++ app/admin/verification/components/Table/ImageDisplay.jsx
@@ -8,20 +8,42 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 import Image from 'next/image'
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Button } from '@/components/ui/button'
 
 export const ImageDisplay = ({ open, setOpen }) => {
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent>
-                <DialogDescription className="" >
-                    <div className="w-full h-full">
-                        <Image
-                            src={"/assets/packageImage/packagePicture.png"}
-                            width={300}
-                            height={300}
-                            alt="img Pict"
-                            style={{ width: '200px', height: '200px' }}
-                        />
+                <DialogDescription>
+                    <div className="flex flex-row gap-4 items-center">
+                        <div className="imageContainer flex flex-col w-[800px] items-center">
+                            <Carousel className="w-full ">
+                                <CarouselContent className=" ">
+                                    {Array.from({ length: 5 }).map((_, index) => (
+                                        <CarouselItem key={index} className=" w-full h-full grow-1">
+                                            <div className="w-full">
+                                                <Image
+                                                    src={'/assets/packageImage/packagePicture.png'}
+                                                    width={500}
+                                                    height={500}
+                                                    alt='package image'
+                                                    className='object-cover w-full rounded-md '
+                                                />
+                                            </div>
+                                        </CarouselItem>
+                                    ))}
+                                </CarouselContent>
+                                <CarouselPrevious className="left-[10px]" />
+                                <CarouselNext className="right-[10px]" />
+                            </Carousel>
+                        </div>
                     </div>
                 </DialogDescription>
             </DialogContent>
