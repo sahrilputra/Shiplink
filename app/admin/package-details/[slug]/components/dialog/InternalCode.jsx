568e32abb9bf116e71fc9a63ac827374f802c41c
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-02-25 03:57:54 +0700

    feature : package Detaills

---
+++ app/admin/package-details/[slug]/components/dialog/InternalCode.jsx
@@ -0,0 +1,114 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState } from "react"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Image from "next/image"
+import Barcode from 'react-jsbarcode';
+import { usePDF } from 'react-to-pdf';
+import ReactToPrint, { useReactToPrint } from "react-to-print"
+import axios from "axios";
+
+
+export function InternalCode({ open, setOpen, trackingID, name, userID }) {
+    const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+    });
+
+    console.log("Tracking ID :", trackingID)
+
+    const [isSkeleton, setIsSkeleton] = useState(true);
+    const [data, setData] = useState([]);
+    console.log("name :", name)
+    const fetchData = async () => {
+        try {
+            const response = await axios.post(
+                `api/admin/arrival_scan/barcode`,
+                data,
+                {
+                    headers: {
+                        Authorization:
+                            `Bearer ${process.env.BEARER_TOKEN}`
+                    }
+                }
+            );
+            console.log(response)
+            setIsSkeleton(false);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        fetchData();
+        setData(trackingID)
+    }, [trackingID]);
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="w-max">
+                <DialogHeader>
+                    <DialogTitle className="text-lg font-bold">Internal Code!</DialogTitle>
+                </DialogHeader>
+
+                <div className="w-max" ref={componentRef}>
+                    <div className="border border-neutral-500/50 rounded-sm p-5 " ref={targetRef}>
+                        <Image
+                            src={"/logo.png"}
+                            width={100}
+                            height={100}
+                            objectFit="contain"
+                            alt="Shiplink Logo"
+                            style={{ width: 100, height: 30, objectFit: "contain" }}
+                        />
+
+                        <div className="flex justify-between w-full flex-row text-sm">
+                            <p>{name}</p>
+                            <p>{`#${userID}`}</p>
+                        </div>
+                        <div className="w-full flex flex-col justify-center items-center p-1">
+                            <img
+                                src={`https://sla.webelectron.com/api/Package/barcode_trackingid?tracking_id=${trackingID}`}
+                                style={{ height: '80px', width: "100%", objectFit: "contain" }}
+                                alt="" />
+                                <p className="text-center tracking-wider py-2">{trackingID}</p>
+                            {/* <Barcode value={`${trackingID}`}
+                                options={{ format: 'code128', width: '3', lineColor: "#2d2d2d", textMargin: 10, fontSize: 16, height: 70 }}
+                                renderer="svg" className="tracking-wider "
+                            /> */}
+                        </div>
+                    </div>
+                </div>
+
+                <DialogFooter>
+                    <Button
+                        variant="redOutline"
+                        className="w-full"
+                        type="submit"
+                        onClick={handlePrint}
+                    >
+                        Print
+                    </Button>
+                    <Button
+                        variant="destructive"
+                        className="w-full"
+                        type="submit"
+                        onClick={() => toPDF()}
+                    >
+                        Download
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
--- app/admin/package-details/[slug]/page.jsx
+++ app/admin/package-details/[slug]/page.jsx
@@ -1,8 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import { Button } from '@/components/ui/button'
-import data from '../../../../data/admin/packageEventData.json'
 import Image from 'next/image'
 import { EventTabled } from './components/EventTable'
 import {
@@ -12,18 +12,53 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel"
+import axios from 'axios'
+import { Skeleton } from '@/components/ui/skeleton'
+import NextLink from 'next/link'
+import { Card } from '@/components/ui/card'
+import { InternalCode } from './components/dialog/InternalCode'
+export default function VerificationPages({ params }) {
+    console.log("Helo", params.slug)
 
-export default function VerificationPages({ props }) {
+    const [openInternal, setOpenInternal] = useState(false);
+    const [data, setData] = useState({});
+    const [skeleton, setSkeleton] = useState(true);
+    const [query, setQuery] = useState({
+        keyword: `${params.slug}`,
+        date_start: "",
+        date_end: "",
+        tracking_id: "",
+    });
 
-    const [open, setOpen] = useState(false);
+    const fetchData = async () => {
+        try {
+            const response = await axios.post(
+                `/api/admin/packages/list`,
+                query
+            );
+            console.log(response)
+            const responseData = await response.data.package_info[0];
+            console.log(responseData)
+            setData(responseData);
+            setSkeleton(false)
+        } catch (error) {
+            setSkeleton(false)
+            console.log('Error:', error);
+        }
+    };
 
-    const [selectedTab, setSelectedTab] = useState("All");
-    console.log("parent : ", selectedTab)
+    useEffect(() => {
+        fetchData();
+    }, [query]);
 
-    const filterData = selectedTab === 'All' ? data : data.filter(item => item.CustomsStatus === selectedTab);
+    console.log("data : ", data)
+    console.log("data : ", data[0]?.customer_name)
 
+    const weigthType = data?.package_weight_unit || "Ibs";
+    const heightType = data?.package_height_unit || "In";
     return (
         <>
+            <InternalCode open={openInternal} setOpen={setOpenInternal} trackingID={data?.tracking_id} name={data?.customer_name} userID={data?.customer_id} />
             <div className={styles.wrapper}>
                 <div className={styles.configHeader}>
                     <div className={styles.banner}>
@@ -50,42 +85,60 @@ export default function VerificationPages({ props }) {
                             <div className="flex flex-col gap-1 py-2">
                                 <div className="profiles flex flex-row justify-start items-center gap-3">
                                     <div className="imgContainer w-[30] h-[30] rounded-full">
-                                        <img src="https://source.boringavatars.com/beam"
-                                            alt="avatar"
-                                            className='w-[30px] h-[30px] rounded-full object-cover'
-                                        />
+                                        {skeleton
+                                            ? <Skeleton className="w-[30px] h-[30px] rounded-full object-cover" />
+                                            : <img src="https://source.boringavatars.com/beam"
+                                                alt="avatar"
+                                                className='w-[30px] h-[30px] rounded-full object-cover'
+                                            />
+                                        }
                                     </div>
                                     <div className="nameContainer">
-                                        <p className='text-sm'>John Doe</p>
-                                        <p className='text-xs text-myBlue'>View User Details</p>
+                                        {skeleton
+                                            ? (
+                                                <>
+                                                    <Skeleton className="w-[100px] h-[20px] rounded-md" />
+                                                </>
+                                            )
+                                            : (
+                                                <>
+                                                    <p className='text-sm'>{data?.customer_name || ""}</p>
+                                                    <NextLink href={`/admin/customers-manager/${data?.customer_id}`}>
+                                                        <p className='text-xs text-myBlue'>View User Details</p>
+                                                    </NextLink>
+
+                                                </>
+                                            )
+                                        }
+
                                     </div>
                                 </div>
 
                                 <div className="packageData flex flex-col gap-1 px-4 py-2">
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Tracking Id</p>
-                                        <p className='text-sm font-bold'>#12134214214</p>
+                                        {skeleton ? <Skeleton className="w-[100px] h-[20px] rounded-md" /> : <p className='text-sm font-bold'>#{data?.tracking_id || ""}</p>}
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Package Status</p>
-                                        <p className='text-sm font-bold'>Forward / Cross Border</p>
+                                        {skeleton ? <Skeleton className="w-[100px] h-[20px] rounded-md" /> : <p className='text-sm font-bold'>{data?.status || ""}</p>}
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Shipping Fees</p>
-                                        <p className='text-sm font-bold'>$ 123.00</p>
+                                        {skeleton ? <Skeleton className="w-[100px] h-[20px] rounded-md" /> : <p className='text-sm font-bold'>$ {data?.shipping_fees || ""}</p>}
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Register Date</p>
-                                        <p className='text-sm font-bold'>12 Apr, 2023</p>
+                                        {skeleton ? <Skeleton className="w-[100px] h-[20px] rounded-md" /> : <p className='text-sm font-bold'>{data?.register_date || "undefined"}</p>}
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Carrier </p>
-                                        <p className='text-sm font-bold'>Feedex</p>
+                                        {skeleton ? <Skeleton className="w-[100px] h-[20px] rounded-md" /> : <p className='text-sm font-bold'>{data?.carrier_code || "undefined"}</p>}
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Location</p>
                                         <p className='text-sm font-bold'>Toronto Warehouse</p>
-                                        <p>#1231 | Row 1 | Section 2 | Lv. 4</p>
+                                        {skeleton ? <Skeleton className="w-[100px] h-[20px] rounded-md" /> : <p className='text-xs'>Bin : {data?.bin_location}</p>}
                                     </div>
 
                                     <Button
@@ -113,6 +166,7 @@ export default function VerificationPages({ props }) {
                                     <Button
                                         variant="secondary"
                                         size="sm"
+                                        onClick={() => setOpenInternal(true)}
                                     >
                                         <p className=' text-xs'>Download Internal Code</p>
                                     </Button>
@@ -126,53 +180,75 @@ export default function VerificationPages({ props }) {
                             </div>
                             <div className="flex flex-row gap-4 py-2">
                                 <div className="imageContainer flex flex-col w-[400px] items-center">
-                                    <Carousel className="w-full ">
-                                        <CarouselContent className=" ">
-                                            {Array.from({ length: 5 }).map((_, index) => (
-                                                <CarouselItem key={index} className=" w-full h-full grow-1">
-                                                    <div className="w-full">
-                                                        <Image
-                                                            src={'/assets/packageImage/packagePicture.png'}
-                                                            width={500}
-                                                            height={500}
-                                                            alt='package image'
-                                                            className='object-cover w-full rounded-md '
-                                                        />
-                                                    </div>
-                                                </CarouselItem>
-                                            ))}
-                                        </CarouselContent>
-                                        <CarouselPrevious className="left-[10px]" />
-                                        <CarouselNext className="right-[10px]" />
-                                    </Carousel>
+                                    {
+                                        skeleton ? (
+                                            <>
+                                                <Skeleton className={"w-[100%] h-[250px]"} />
+                                            </>
+                                        ) : (
+                                            <Carousel className="w-full ">
+                                                <CarouselContent className=" ">
+                                                    <>
+                                                        {Array.from({ length: data?.images.length }).map((_, index) => (
+                                                            <CarouselItem key={index} className=" w-full h-full grow-1">
+                                                                <div className="w-full">
+                                                                    <Card>
+                                                                        <img
+                                                                            style={{ objectFit: "cover", width: '100%', height: '250px', borderRadius: '8px' }}
+                                                                            src={`https://sla.webelectron.com/api/Package/getimages?fullName=${data?.images[index].images}`}
+                                                                            alt=""
+                                                                        />
+                                                                    </Card>
+
+                                                                </div>
+                                                            </CarouselItem>
+                                                        ))}
+                                                    </>
+
+
+                                                </CarouselContent>
+                                                <CarouselPrevious className="left-[10px]" />
+                                                <CarouselNext className="right-[10px]" />
+                                            </Carousel>
+                                        )
+                                    }
 
                                 </div>
 
                                 <div className="flex flex-col ">
-                                    <p className="text-red-700 text-opacity-80 text-lg font-bold font-['Poppins']">#872812138328</p>
+                                    <div className="text-red-700 text-opacity-80 text-lg font-bold font-['Poppins']">
+                                        {skeleton
+                                            ? <Skeleton />
+                                            : (
+                                                <>
+                                                    #{data?.barcode_tracking}
+                                                </>
+                                            )
+                                        }
+                                    </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Package Weight</p>
-                                        <p className='text-sm font-bold'>12 Ibs</p>
+                                        <p className='text-sm font-bold'>{data?.package_weight} {data?.package_weight_unit}</p>
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Package Size</p>
-                                        <p className='text-sm font-bold'>4 x 5 x 4 In</p>
+                                        <p className='text-sm font-bold'>{data?.package_length} x {data?.package_witdth} x {data?.package_height}  {data?.package_height_unit}</p>
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Total Item Price</p>
-                                        <p className='text-sm font-bold'>$ 129.99</p>
+                                        <p className='text-sm font-bold'>$ {data?.total_price}</p>
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>PARS</p>
-                                        <p className='text-sm font-bold'>1234241241</p>
+                                        <p className='text-sm font-bold'>{data?.pars || "undefined"}</p>
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Entry Number</p>
-                                        <p className='text-sm font-bold'>12314-21321-3232</p>
+                                        <p className='text-sm font-bold'>{data?.entry_number || "undefined"}</p>
                                     </div>
                                     <div className="flex flex-col text-xs text-zinc-500">
                                         <p>Manifest Number</p>
-                                        <p className='text-sm font-bold'>1231112</p>
+                                        <p className='text-sm font-bold'>{data?.manifest || "undefined"}</p>
                                     </div>
                                 </div>
                             </div>
@@ -183,7 +259,7 @@ export default function VerificationPages({ props }) {
                     <div className="head">
                         <p className=' text-myBlue font-base font-bold'>Package History</p>
                     </div>
-                    <EventTabled data={data} />
+                    {/* <EventTabled data={data} /> */}
                 </div>
             </div>
         </>
