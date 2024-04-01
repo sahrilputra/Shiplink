6e6b8931c07a5f8e1c7f72dd29ef62ff17d3cbd9
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-04-01 23:15:08 +0700

    Fix bug in login functionality

--- app/admin/package-details/[slug]/page.jsx
+++ app/admin/package-details/[slug]/page.jsx
@@ -240,7 +240,7 @@ export default function VerificationPages({ params }) {
                                             <div className="flex flex-row gap-1 items-center">
                                                 <img src={`https://flagcdn.com/${countryCode}.svg`} alt="country icon" style={{ objectFit: 'fill', width: '25px', height: '25px' }} />
                                                 <p className='text-sm font-bold'>
-                                                    {data?.warehouse_name_arrival} WR - {data?.country_name_arrival}
+                                                    {data?.warehouse_name_arrival} WH - {data?.country_name_arrival}
                                                 </p>
                                             </div>
                                         )
@@ -252,7 +252,7 @@ export default function VerificationPages({ params }) {
                                             <div className="flex flex-row gap-1 items-center">
                                                 <img src={`https://flagcdn.com/${countryCodeArrival}.svg`} alt="country icon" style={{ objectFit: 'fill', width: '25px', height: '25px' }} />
                                                 <p className='text-sm font-bold text-nowrap'>
-                                                    {data?.warehouse_name_destination} WR - {data?.country_name_destination}
+                                                    {data?.warehouse_name_destination} WH - {data?.country_name_destination}
                                                 </p>
                                             </div>
                                         )
