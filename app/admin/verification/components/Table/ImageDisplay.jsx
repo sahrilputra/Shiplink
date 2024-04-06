b73ece9d165dd822b6410ced813888d8fd97f49d
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-04-06 09:02:53 +0700

    #issues : 100 zooming images

--- app/admin/verification/components/Table/ImageDisplay.jsx
+++ app/admin/verification/components/Table/ImageDisplay.jsx
@@ -16,36 +16,44 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel"
+import Magnifier from "react-magnifier";
 import { Button } from '@/components/ui/button'
+
 
 export const ImageDisplay = ({ open, setOpen, images = null }) => {
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogContent>
-                <DialogDescription>
-                    <div className="flex flex-row gap-4 items-center">
-                        <div className="imageContainer flex flex-col w-[600px]  items-center">
-                            <Carousel className="w-full ">
-                                <CarouselContent className=" ">
-                                    {Array.from({ length: images?.length }).map((_, index) => (
-                                        <CarouselItem key={index} className=" w-full h-full grow-1">
-                                            <div className="w-full">
-                                                <img
-                                                    className='object-cover w-full h-[350px] rounded-md '
-                                                    src={`https://sla.webelectron.com/api/Package/getimages?fullName=${images[index].images}`}
-                                                    alt="" />
-                                            </div>
-                                        </CarouselItem>
-                                    ))}
-                                </CarouselContent>
-                                <CarouselPrevious className="left-[10px]" />
-                                <CarouselNext className="right-[10px]" />
-                            </Carousel>
+        <>
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent>
+                    <DialogDescription>
+                        <div className="flex flex-row gap-4 items-center">
+                            <div className="imageContainer flex flex-col w-[600px]  items-center">
+                                <Carousel className="w-full ">
+                                    <CarouselContent className=" ">
+                                        {Array.from({ length: images?.length }).map((_, index) => (
+                                            <CarouselItem key={index} className=" w-full h-full grow-1">
+                                                <div className="w-full">
+                                                    <Magnifier
+                                                        className='image'
+                                                        src={`https://sla.webelectron.com/api/Package/getimages?fullName=${images[index].images}`}
+                                                        alt=""
+                                                        width={600}
+                                                        height={400}
+                                                    />
+                                                </div>
+                                            </CarouselItem>
+                                        ))}
+                                    </CarouselContent>
+                                    <CarouselPrevious className="left-[10px]" />
+                                    <CarouselNext className="right-[10px]" />
+                                </Carousel>
 
+                            </div>
                         </div>
-                    </div>
-                </DialogDescription>
-            </DialogContent>
-        </Dialog>
+                    </DialogDescription>
+                </DialogContent>
+            </Dialog>
+        </>
+
     )
 }
