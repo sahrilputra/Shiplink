67699dc30b872cafe7ed7d2f90b96da35e23aceb
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-04-12 01:16:48 +0700

    Refactor LotsHead component to use smaller font sizes and adjust spacing

--- app/view/lots/components/Docs/LotsHead.jsx
+++ app/view/lots/components/Docs/LotsHead.jsx
@@ -18,36 +18,36 @@ export const LotsHead = ({ data }) => {
 
                 {/* Lot Details */}
                 <View style={{ display: 'flex', flexDirection: "row", justifyContent: "space-between", width: "100%", alignItems: 'flex-end' }}>
-                    <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+                    <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', gap: "2px" }}>
                         <View style={{ display: 'flex', justifyContent: "flex-start", gap: '3px', alignItems: 'center', flexDirection: 'row' }}>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px', width: '100px' }}>Lot ID </Text>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px' }}>:</Text>
-                            <Text style={{ fontWeight: 'light', color: '#5A5A5A', fontSize: '12px' }}>{data?.lots_id}</Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px', width: '100px' }}>Lot ID </Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px' }}>:</Text>
+                            <Text style={{ fontWeight: 'light', color: '#5A5A5A', fontSize: '11px' }}>{data?.lots_id}</Text>
                         </View>
                         <View style={{ display: 'flex', justifyContent: "flex-start", gap: '3px', alignItems: 'center', flexDirection: 'row' }}>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px', width: '100px' }}>Origin </Text>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px' }}>:</Text>
-                            <Text style={{ fontWeight: 'light', color: '#5A5A5A', fontSize: '12px' }}>{data?.warehouse_origin_name} WH - {data?.country_name}</Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px', width: '100px' }}>Origin </Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px' }}>:</Text>
+                            <Text style={{ fontWeight: 'light', color: '#5A5A5A', fontSize: '11px' }}>{data?.warehouse_origin_name} WH - {data?.country_name}</Text>
                         </View>
                         <View style={{ display: 'flex', justifyContent: "flex-start", gap: '3px', alignItems: 'center', flexDirection: 'row' }}>
-                            <Text style={{ fontWeight: 'extrabold', fontSize: '12px', width: '100px' }}>Destination</Text>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px' }}>:</Text>
-                            <Text style={{ fontWeight: 'extrabold', color: '#5A5A5A', fontSize: '12px' }}>{data?.warehouse_destination_name} WH - {data?.destination_name}</Text>
+                            <Text style={{ fontWeight: 'extrabold', fontSize: '11px', width: '100px' }}>Destination</Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px' }}>:</Text>
+                            <Text style={{ fontWeight: 'extrabold', color: '#5A5A5A', fontSize: '11px' }}>{data?.warehouse_destination_name} WH - {data?.destination_name}</Text>
                         </View>
                         <View style={{ display: 'flex', justifyContent: "flex-start", gap: '3px', alignItems: 'center', flexDirection: 'row' }}>
-                            <Text style={{ fontWeight: 'extrabold', fontSize: '12px', width: '100px' }}>Trip Number</Text>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px' }}>:</Text>
-                            <Text style={{ fontWeight: 'extrabold', color: '#5A5A5A', fontSize: '12px' }}>{data?.trip_number}</Text>
+                            <Text style={{ fontWeight: 'extrabold', fontSize: '11px', width: '100px' }}>Trip Number</Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px' }}>:</Text>
+                            <Text style={{ fontWeight: 'extrabold', color: '#5A5A5A', fontSize: '11px' }}>{data?.trip_number}</Text>
                         </View>
                         <View style={{ display: 'flex', justifyContent: "flex-start", gap: '3px', alignItems: 'center', flexDirection: 'row' }}>
-                            <Text style={{ fontWeight: 'extrabold', fontSize: '12px', width: '100px' }}>Pickup Schedule</Text>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px' }}>:</Text>
-                            <Text style={{ fontWeight: '500', color: '#5A5A5A', fontSize: '12px' }}>{data?.pickup_schedule}</Text>
+                            <Text style={{ fontWeight: 'extrabold', fontSize: '11px', width: '100px' }}>Pickup Schedule</Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px' }}>:</Text>
+                            <Text style={{ fontWeight: '500', color: '#5A5A5A', fontSize: '11px' }}>{data?.pickup_schedule}</Text>
                         </View>
                         <View style={{ display: 'flex', justifyContent: "flex-start", gap: '3px', alignItems: 'center', flexDirection: 'row' }}>
-                            <Text style={{ fontWeight: 'extrabold', fontSize: '12px', width: '100px' }}>Total Items</Text>
-                            <Text style={{ fontWeight: 'bold', fontSize: '12px' }}>:</Text>
-                            <Text style={{ fontWeight: '500', color: '#5A5A5A', fontSize: '12px' }}>{data?.total_items}</Text>
+                            <Text style={{ fontWeight: 'extrabold', fontSize: '11px', width: '100px' }}>Total Items</Text>
+                            <Text style={{ fontWeight: 'bold', fontSize: '11px' }}>:</Text>
+                            <Text style={{ fontWeight: '500', color: '#5A5A5A', fontSize: '11px' }}>{data?.total_items}</Text>
                         </View>
                     </View>
                 </View>
