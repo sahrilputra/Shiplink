9722b7d2b44722a4a741605845a22958700b6cb7
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-04-19 01:58:37 +0700

    Initial New Icons

--- components/sidebar/AdminSidebar.jsx
+++ components/sidebar/AdminSidebar.jsx
@@ -255,3 +255,207 @@ export const AdminSidebar = () => {
     </>
   );
 };
+
+
+// <ScrollArea className="h-[100vh]">
+// <div className={` flex gap-3 flex-col pb-[30px] `}>
+//   <div className="h-[50px]">
+//     <div
+//       className={`${styles.logo} fixed bg-white mb-[10px] text-center h-[50px] w-[230px] flex flex-col gap-2 justify-items-center z-[25]`}
+//     >
+//       <div className=" w-max h-max mx-auto bg-white ">
+//         <Image
+//           src={"/logo.png"}
+//           width={120}
+//           height={120}
+//           alt="shiplink Logo"
+//           className="mx-auto"
+//           style={{ width: "120px", height: "30px" }}
+//         />
+//         <div className="w-[200px] mt-2 flex items-center justify-center mx-auto">
+//           <Separator className="h-[2px]" />
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+
+//   <SidebarMenu
+//     title=""
+//     className="flex-col justify-start items-center flex "
+//   >
+//     <div className="flex-col justify-end items-start gap-[5px] flex">
+//       <SidebarItem
+//         isActive={router === "/admin/package-details"}
+//         title="Dashboard"
+//         icon={<Package2 width={22} height={22} />}
+//         href="/admin/package-details"
+//       />
+
+//       <SidebarItem
+//         isActive={router === "/admin/arrival-scan"}
+//         title="Arrival Scan"
+//         icon={<ArrivalIcon width={20} height={20} />}
+//         href="/admin/arrival-scan"
+//       />
+
+//       <SidebarItem
+//         isActive={router === "/admin/verification"}
+//         title="Verification"
+//         icon={<PackageCheck width={22} height={22} />}
+//         href="/admin/verification"
+//       />
+
+//       <SidebarItem
+//         isActive={router === "/admin/custom-brokers"}
+//         title="Customs Broker"
+//         icon={<Stamp width={22} height={22} />}
+//         href="/admin/custom-brokers"
+//       />
+
+//       <SidebarItem
+//         isActive={router.startsWith("/admin/transport")}
+//         title="Transport"
+//         icon={<Truck width={22} height={22} />}
+//         href="/admin/transport"
+//       />
+
+//       <SidebarItem
+//         isActive={router === "/admin/custom-clearance"}
+//         title="Custom Clearance"
+//         icon={
+//           <Megaphone
+//             width={22}
+//             height={22}
+//             className="relative -rotate-12"
+//           />
+//         }
+//         href="/admin/custom-clearance"
+//       />
+//       <SidebarItem
+//         isActive={router === "/admin/destination"}
+//         title="Destination Scan"
+//         icon={
+//           <PackageSearchIcon
+//             width={22}
+//             height={22}
+//             className="relative"
+//           />
+//         }
+//         href="/admin/destination"
+//       />
+//       <SidebarItem
+//         isActive={router === "/admin/purchase-manager"}
+//         title="Assisted Purchase"
+//         icon={
+//           <ShoppingBag
+//             width={22}
+//             height={22}
+//             className="relative"
+//           />
+//         }
+//         href="/admin/purchase-manager"
+//       />
+//       <SidebarItem
+//         isActive={router.startsWith("/admin/customers-manager")}
+//         title="Customers"
+//         icon={<UsersRound width={23} height={23} />}
+//         href="/admin/customers-manager"
+//       />
+
+//       <SidebarItem
+//         isActive={router === "/admin/invoice-manager"}
+//         title="Invoices"
+//         icon={
+//           <Newspaper 
+//             width={22}
+//             height={22}
+//             className="relative"
+//           />
+//         }
+//         href="/admin/invoice-manager"
+//       />
+
+//       <SidebarItem
+//         isActive={router.startsWith("/admin/product-manager")}
+//         title="Products & Services"
+//         icon={
+//           <ProductManagerIcon
+//             width={23}
+//             height={23}
+//             className="relative"
+//           />
+//         }
+//         href="/admin/product-manager"
+//       />
+
+//       <SidebarItem
+//         isActive={router === "/admin/bin-management"}
+//         title="Bin Manager"
+//         icon={
+//           <BinManagerIcon
+//             width={23}
+//             height={23}
+//             className="relative"
+//           />
+//         }
+//         href="/admin/bin-management"
+//       />
+
+//       <SidebarItem
+//         isActive={router.startsWith("/admin/warehouse-manager")}
+//         title="Warehouses"
+//         icon={
+//           <WarehouseIcons
+//             width={23}
+//             height={23}
+//             className="relative"
+//           />
+//         }
+//         href="/admin/warehouse-manager"
+//       />
+
+//       <SidebarItem
+//         isActive={router === "/admin/support-tickets"}
+//         title="Support Tickets"
+//         isLast="true"
+//         icon={
+//           <SupportTickets
+//             width={23}
+//             height={23}
+//             className="relative"
+//           />
+//         }
+//         href="/admin/support-tickets"
+//       />
+//     </div>
+//   </SidebarMenu>
+//   <SidebarMenu
+//     title="setting"
+//     className="flex-col justify-start items-center  flex"
+//   >
+//     <div className="flex-col justify-end items-start gap-[5px] flex">
+//       <SidebarItem
+//         onClick={toggleClicked}
+//         isClicked={isClicked}
+//         isActive={router.startsWith("/admin/PARS")}
+//         title="PARS & PAPS"
+//         icon={<ParsIcon className={"h-[25px] w-[25px]"} />}
+//         href="/admin/PARS"
+//       />
+//       <SidebarItem
+//         isActive={router.startsWith("/admin/user-permission")}
+//         title="Users & Permissions"
+//         icon={<UserPemissionIcon width={23} height={23} />}
+//         href="/admin/user-permission"
+//       />
+//       <SidebarItem
+//         isActive={router.startsWith("/admin/configuration")}
+//         title="Configuration"
+//         isLast="true"
+//         icon={<ConfigIcon width={23} height={23} />}
+//         href="/admin/configuration"
+//       />
+//     </div>
+//   </SidebarMenu>
+// </div>
+// </ScrollArea>
