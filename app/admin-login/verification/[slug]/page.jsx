deefef2111395f9078e4958a62c6134f163d3d8e
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-04-25 20:21:32 +0700

    fixing verification pages

--- app/admin-login/verification/[slug]/page.jsx
+++ app/admin-login/verification/[slug]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import NextLink from 'next/link';
 import https from "https";
 const agent = new https.Agent({
